31b9e26383595dff0011331075639b21227c7c62
Yoshua Isreal <yoshtown@gmail.com>
2026-02-17 12:47:43 -0500

    update

--- api/app.py
+++ api/app.py
@@ -26,14 +26,14 @@ def predict():
 
 		# Predict
 		prediction = pipeline.predict([text])
-		probabilities = pipeline.predict_probab([text])
+		# probabilities = pipeline.predict_probab([text])
 
 		# Map prediction to label
 		labels = ['Beginner', 'Intermediate', 'Advanced']
 
 		return jsonify({
-			'prediction': prediction,
-			'confidence': float(max(probabilities))
+			'prediction': prediction
+			# 'confidence': float(max(probabilities))
 			# 'probabilities': {
             #     labels[i]: float(probabilities[i])
             #     for i in range(len(probabilities))
